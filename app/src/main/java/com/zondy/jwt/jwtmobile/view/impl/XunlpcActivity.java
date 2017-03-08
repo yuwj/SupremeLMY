73912c74e27840660793e21ce8d3031d7f840ec3
acesheep <912515521@qq.com>
2017-03-08 15:59:58 +0800

    2017年03月08日15:59:42 增加时间选择

--- app/src/main/java/com/zondy/jwt/jwtmobile/view/impl/XunlpcActivity.java
+++ app/src/main/java/com/zondy/jwt/jwtmobile/view/impl/XunlpcActivity.java
@@ -9,10 +9,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.zondy.jwt.jwtmobile.R;
 import com.zondy.jwt.jwtmobile.base.BaseActivity;
 import com.zondy.jwt.jwtmobile.util.ToastTool;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 
@@ -20,12 +25,18 @@ import butterknife.BindView;
  * Created by sheep on 2017/3/2.
  */
 
-public class XunlpcActivity extends BaseActivity implements View.OnClickListener{
+public class XunlpcActivity extends BaseActivity implements View.OnClickListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.btn_xunlpc_search)
     Button btnXunlpcSearch;
+    @BindView(R.id.rl_kaisrq)
+    RelativeLayout rlKaisrq;
+    @BindView(R.id.tv_kaisrq)
+    TextView tvKaisrq;
+    @BindView(R.id.tv_jiezrq)
+    TextView tvJiezrq;
 
     @Override
     public int setCustomContentViewResourceId() {
@@ -45,13 +56,20 @@ public class XunlpcActivity extends BaseActivity implements View.OnClickListener
     }
 
     private void initParam() {
-
+        Date date = new Date(System.currentTimeMillis());
+        Date date1 = new Date(System.currentTimeMillis() - 604800000);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String jzrq = sdf.format(date);
+        String ksrq = sdf.format(date1);
+        tvJiezrq.setText(jzrq);
+        tvKaisrq.setText(ksrq);
     }
 
     private void initView() {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         btnXunlpcSearch.setOnClickListener(this);
+        rlKaisrq.setOnClickListener(this);
     }
 
     @Override
@@ -75,9 +93,13 @@ public class XunlpcActivity extends BaseActivity implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_xunlpc_search:
                 XunlpcRycxlistActivity.actionStart(XunlpcActivity.this);
+                break;
+            case R.id.rl_kaisrq:
+                Intent intent = new Intent(XunlpcActivity.this, CalendarSelectorActivity.class);
+                startActivity(intent);
                 break;
         }
     }
